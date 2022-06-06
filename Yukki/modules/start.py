66ec03b2218649e6390372f67ac04802e24dcf35
ʜᴇʀᴏx _ xᴅ <103039834+HeroX-xD@users.noreply.github.com>
2022-06-06 15:12:54 +0530

    Update start.py

--- Yukki/modules/start.py
+++ Yukki/modules/start.py
@@ -17,7 +17,6 @@ from Yukki.helpers import get_readable_time
 
 
 @app.on_message(filters.command(["start", "ping"]))
-async def start_private(client: Client, message: Message):
  await message.reply_photo(
         photo=f"https://telegra.ph/file/a82f511eb98f58a685e32.jpg",
         caption=f"""ʜᴇʟʟᴏ✨ **ᴡᴇʟᴄᴏᴍᴇ {message.from_user.mention()} !**\n
